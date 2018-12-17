6e867ba6e1756352c1d98e259169720651c8cc8d
Ammon Shepherd <ammon.uva@gmail.com>
2018-12-17 13:35:48 -0500

    Use compose for solr and add search via javascript
    This update finishes off the functionality of the site. This biggest
    factor is the use of docker-compose to fully build and load data into
    the solr container (instead of doing things manually and building an
    image from that).
    
    This also completes the search functionality using solrSearch.js.

--- README.md
+++ README.md
@@ -1,6 +1,75 @@
-This scrapes the old faulkner site built with Apache Cocoon and Solr, and converts it to plain HTML/CSS and Solr(?) or maybe some other search product.
+This repo contains the files scraped from the old latvian site built with
+Apache Cocoon and Solr, and converted to plain HTML/CSS and Solr.
 
-To scrape the old site:
-- `wget --mirror --no-host-directories --no-parent --page-requisites --convert-links --convert-file-only --adjust-extension --directory-prefix=static-content http://latviandainas.lib.virginia.edu`
+********************************
+# Process for creating a clone
 
-TODO:
+These were the steps used to recreate the old production site from Apache Cocoon and Solr.
+
+It consists of using docker-compose with two images that work together to
+provide static files and the Solr search.  The basic steps required to create
+the images are outlined below, with detailed steps following.
+
+- STATIC container: 
+  - scrape the original website
+  - copy any unlinked files
+  - use the official nginx image with a local volume to copy in the modified default.conf file
+  - modify the default.conf nginx file
+  - change the search page to work with solr via javascript
+  - create a javascript file to connect the search page and solr search engine
+- SOLR container:
+  - use the official solr image
+  - change the managed-schema file
+  - change the web.xml file
+  - create a load-data script
+  - have all the xml data files in place
+
+## Create the STATIC container
+- Scrape the old site:
+    - `wget --mirror --no-host-directories --no-parent --page-requisites --convert-links --convert-file-only --adjust-extension --directory-prefix=static-content http://latviandainas.lib.virginia.edu`
+- Edit the default nginx.conf file to serve files (and take care of file names
+  with '?' in them.)
+
+## Create the SOLR container
+This uses a Dockerized solr to get an up to date solr and keep the search
+functionality.
+
+This uses a default solr docker image, and just plugs in some config files and
+imports data into the core after the docker image is up and running.
+
+The updated config files are:
+- managed-schema
+- web.xml
+
+This set up uses a modified default solr script to create a core and import the
+data into the index. `load-data` script is a modified copy of the solr script
+`solr-demo` from
+https://github.com/docker-solr/docker-solr/blob/master/scripts/solr-demo
+
+The data is in the folder `add_docs` and are the xml file from the old server
+that were used to build the index originally.
+
+The solr container is built using docker-compose. There is a container for
+nginx to serve the static files from the `static_content` directory. The solr
+container is built from a default solr image and loads the config files and the
+data via local volumes. The magic happens by calling the entrypoint section,
+which determines the command and its parameters to be executed when the container
+is run. In our case it calls solr's default docker-entrypoint.sh script which
+just looks for a parameter of a named script to run from the
+`/opt/docker-solr/scripts/` directory. This is where we place our modified
+`solr-demo` script. This script is also looking for a parameter, the name of
+the core it should create. This script runs solr, creates a default core, stops
+solr, copies the config files from the local volumes into place over the
+default files, restarts solr and loads the data into the index, stops solr,
+then restarts solr in the foreground.
+
+So the process goes:
+execute `docker-entrypoint.sh` -> which runs `load-data` script which does all
+the magic of creating a core and loading the data.
+
+This happens each time the container is run.
+
+
+# Development
+- A docker-compose-dev.yml file is available for use locally or on production
+  when traefik is used.
---
+++ static-content/solrSearch.js
@@ -0,0 +1,160 @@
+
+
+//const query = 'http://faulkner.lib.virginia.edu:8899/solr/fcore/select?df=fulltext&rows=2000&fq=type:transcription&q=fulltext:lawn'
+//const theURL = 'http://latviandainas.lib.virginia.edu:8983/solr/latcore/select?df=fulltext&rows=80000&'
+const theURL = 'http://latviandainas.lib.virginia.edu:8983/solr/latcore/select?rows=80000&'
+
+
+function xhrSuccess() { 
+    this.callback.apply(this, this.arguments); 
+    removeLoader();
+}
+
+function xhrError() { 
+    console.error(this.statusText); 
+}
+
+function loadResults(url, callback /*, opt_arg1, opt_arg2, ... */) {
+    const xhr = new XMLHttpRequest();
+    xhr.callback = callback;
+    xhr.arguments = Array.prototype.slice.call(arguments, 2);
+    xhr.onload = xhrSuccess;
+    xhr.onerror = xhrError;
+    xhr.open("GET", url, true);
+    xhr.send(null);
+}
+
+function showMessage(message) {
+    console.log(message + this.responseText);
+}
+
+function querySolr() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get('action');
+    //if ("Submit" == myParam ) {
+    if (window.location.search.indexOf('action')) {
+        const query = buildQueryURL();
+        loadResults(query, displayResult);
+    } else {
+        removeLoader();
+    }
+}
+
+function buildQueryURL() {
+
+    const urlParams = new URLSearchParams(window.location.search)
+    const fulltext = urlParams.get('searchText');
+
+    //const query = theURL + 'hl=on&hl.fl=fulltext&fq=type:' + type + '&q=fulltext:"' + fulltext + '"'
+    const query = theURL + 'q=fulltext:' + fulltext
+
+    return query;
+}
+
+/** Displays the results from solr.
+  * Manipulates the search.html page, adding a div to display the number of
+  * results, and a list of each item from the search results.
+  *
+  * @param {object} searchRequest - The JSON object returned by the solr database; the search results.
+*/
+function displayResult() {
+    const jObj = JSON.parse(this.response);
+    const resultDiv = document.getElementById("searchResult");
+
+    const numResults = document.createElement("div");
+    resultDiv.appendChild(numResults);
+    numResults.innerHTML = "Results Found: " + jObj.response.numFound;
+
+    const resultList = document.createElement("ul");
+    resultDiv.appendChild(resultList);
+    var theClass = "result_div_odd";
+    jObj.response.docs.forEach(function(item, index){
+        if ( (index+1)%2 == 0 ){
+            theClass = "result_div_even";
+        } else {
+            theClass = "result_div_odd";
+        }
+
+        //var highlight = jObj.highlighting[item.id].fulltext;
+        // need to figure out lang variable
+        var volume = setVolume(item.volume_id);
+        var htmlStuff = 
+            '<li class="'+ theClass +'">' +
+                '<p>' +
+                '<a href="tei.'+ item.volume_id +'.xml?lang=eng&amp;div_id='+ item.id +'">'+ item.lg_head +'</a>' +
+                '<br> From <a href="tei.'+ item.volume_id +'.xml?lang=eng&amp;div_id='+ item.parent_id +'">'+ item.parent_head +'</a>, '+ volume +'</p>' +
+            '</li>';
+
+
+        resultList.innerHTML += htmlStuff;
+    });
+}
+
+function removeLoader() {
+    var loader = document.getElementById('loader');
+    loader.remove();
+}
+
+
+function setVolume(volume_id) {
+    var volume = {
+        'latv01': 'I',
+        'latv02': 'II',
+        'latv03': 'III',
+        'latv04': 'IV',
+        'latv05': 'V',
+        'latv06': 'VI',
+        'latv07': 'VII',
+        'latv08': 'VIII',
+        'latv09': 'IX',
+        'latv10': 'X',
+        'latv11': 'XI',
+        'latv12': 'XII'
+    };
+
+    return "Volume " + (volume[volume_id]);
+}
+
+
+
+// One time use function to create a temporary page (grover/grover.html) that
+// lists all of the records from the solr database. The list is then saved to
+// the production site so that each of the individual bibliography pages are
+// linked to, which will then allow the scrape to generate a static file for
+// each bib. page.
+/*
+function allFromSolr() {
+  var query = theURL + "q=*:*";
+  if (request) {
+    request.open('GET', query, true);
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+          var jObj = JSON.parse(request.response);
+          var resultDiv = document.getElementById("allResults");
+
+          var numResults = document.createElement("div");
+          resultDiv.appendChild(numResults);
+          numResults.innerHTML = "Results Found: " + jObj.response.numFound;
+
+          var resultList = document.createElement("ul");
+          resultDiv.appendChild(resultList);
+          var theClass = "result_odd";
+          jObj.response.docs.forEach(function(item, index){
+            if ( (index+1)%2 == 0 ){
+              theClass = "result_even";
+            } else {
+              theClass = "result_odd";
+            }
+
+            var text = item.id + ". " + item.title;
+            resultList.innerHTML += "<li class='" + theClass + "'>" + "<a href='http://faulkner.lib.virginia.edu/media%3fid=" + item.id.trim() + "'>" + text + "</a></li>";
+          });
+
+      }
+    }
+    request.send();
+  } else {
+    document.getElementById("searchError").textContent = "There was a problem connecting to solr.";
+  }
+}
+*/
